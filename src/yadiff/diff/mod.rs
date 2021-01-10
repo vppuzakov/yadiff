4d55e03708b381896b180d476b51c22ccc56f135
Vladimir Puzakov <vppuzakov@gmail.com>
2021-01-11 01:53:08 +0300

    diff: get file hashsums

---
+++ src/yadiff/diff/mod.rs
@@ -0,0 +1,5 @@
+pub use self::client::{from_local, from_yadisk};
+pub use self::models::File;
+
+pub mod client;
+pub mod models;
--- src/yadiff/lib.rs
+++ src/yadiff/lib.rs
@@ -1,2 +1,3 @@
+pub mod diff;
 pub mod filescanner;
 pub mod yadisk;
