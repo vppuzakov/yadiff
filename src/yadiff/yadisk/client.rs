eef281ce8c9d27339a13950884d20a242c26b7d9
Vladimir Puzakov <vppuzakov@gmail.com>
2021-01-10 04:08:10 +0300

    feat: load all resources from folder

--- src/yadiff/yadisk/client.rs
+++ src/yadiff/yadisk/client.rs
@@ -3,12 +3,14 @@ use reqwest::Error;
 
 use crate::yadisk::models::Resource;
 
-pub async fn get_resource(token: String, path: String) -> Result<(), Error> {
+pub async fn get_resource(token: &String, path: &String, limit: u32, offset: u32) -> Result<Resource, Error> {
     let client = reqwest::Client::new();
 
     let request_url = format!(
-        "https://cloud-api.yandex.net/v1/disk/resources?path={path}",
+        "https://cloud-api.yandex.net/v1/disk/resources?path={path}&limit={limit}&offset={offset}",
         path = path,
+        limit = limit,
+        offset = offset,
     );
 
     let response = client
@@ -20,6 +22,25 @@ pub async fn get_resource(token: String, path: String) -> Result<(), Error> {
         .await?;
 
     let resource: Resource = response.json().await?;
-    println!("{:?}", resource);
-    Ok(())
+    Ok(resource)
+}
+
+pub async fn get_all_files(token: &String, path: &String, limit: u32) -> Result<Vec<Resource>, Error> {
+    let mut files: Vec<Resource> = Vec::new();
+    let mut offset = 0;
+
+    let mut folder = get_resource(&token, &path, 1, 0).await?;
+    let mut embedded = folder.embedded.unwrap();
+    let total = embedded.total;
+
+    while offset < total {
+        folder = get_resource(&token, &path, limit, offset).await?;
+        embedded = folder.embedded.unwrap();
+        for item in embedded.items {
+            files.push(item);
+        }
+        offset += limit;
+    }
+
+    Ok(files)
 }
